a3eab0951c45ec6b806ae60c9adf14b56873aee4
vikashraj144 <vikashraj144@gmail.com>
2018-03-13 18:30:45 +0530

    sigleton pattern

---
+++ tutorial/Design-patterns/singleton-pattern.js
@@ -0,0 +1,29 @@
+/* 
+the singleton pattern can be implemented by
+creating a class with a method that creates a new instance of the class if one doesn't exist. 
+
+it restricts instantiation of a class to a single object.
+*/
+
+    var Singleton = (function () {
+        var instantiated;
+        function init() {
+        // singleton here
+        return {
+        publicMethod: function () {
+        console.log('hello world');
+        },
+        publicProperty: 'test'
+        };
+        }
+        return {
+        getInstance: function () {
+        if (!instantiated) {
+        instantiated = init();
+        }
+        return instantiated;
+        }
+        };
+        })();
+        // calling public methods is then as easy as:
+        Singleton.getInstance().publicMethod();
