6ca9756437806161df28aaf36627605ae5df7858
vikashraj144 <vikashraj144@gmail.com>
2018-03-13 18:00:01 +0530

    observer-pattern

---
+++ tutorial/Design-patterns/observer-pattern.js
@@ -0,0 +1,13 @@
+/* 
+In an event-driven environment, such as the browser where it is constantly seeking attention
+from a user, the observer pattern, also known as the publisher-subscriber pattern,
+ 
+In the observer pattern, there are two roles: the observer and the observed (watch or be
+watched)
+Example: Newspaper  Delivery
+
+- Subscribers can subscribe and unsubscribe. They also receive. They have the option of
+being delivered to, or receiving for themselves.
+- Publishers deliver. They have the option of giving, or being taken from.
+
+*/
