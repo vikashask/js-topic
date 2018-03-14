1163186688829a4d2ee8008c8794505cf86db945
vikashraj144 <vikashraj144@gmail.com>
2018-03-14 11:06:39 +0530

    added object

--- object.js
+++ object.js
@@ -4,34 +4,34 @@ following ways to create objects in JavaScript :
 
 // 1. The simplest way to create an object is to use the Object constructor
 
-var person = new Object();  
-person.name = "vikash";  
-person.getName = function(){  
-    return this.name;  
-}; 
+var person = new Object();
+person.name = "vikash";
+person.getName = function () {
+    return this.name;
+};
 
 // 2. Literal notation
 
-var person = {  
-    person:name = "vikash",
-    person:getName = function(){  
-        return this.name;  
-    }  
-} 
+var person = {
+    person: name = "vikash",
+    person: getName = function () {
+        return this.name;
+    }
+}
 /* 3. Factory function
 
 The Factory function allows to encapsulate and re-use the logic for creating similar objects. 
 */
-var newPerson=function(name){  
-    var result = new Object();  
-    result.name = name;  
-    result.getName = function(){  
-        return this.name;  
-    };  
-    return result;  
-};  
-var personOne = newPerson("Diego");  
-var personTwo = newPerson("Gangelo");  
+var newPerson = function (name) {
+    var result = new Object();
+    result.name = name;
+    result.getName = function () {
+        return this.name;
+    };
+    return result;
+};
+var personOne = newPerson("Diego");
+var personTwo = newPerson("Gangelo");
 console.log(personOne.getName()); // prints Diego  
 console.log(personTwo.getName()); // prints Gangelo
 
@@ -41,13 +41,13 @@ In Javascript it is possible to call any function with the new operator in front
 Given a function F, for new F(): a new empty object X is created.
 X is set as context for F meaning throughout F this points to X. X is returned as result of F
  */
-function Person(name){  
-        this.name = name;  
-        this.getName = function(){  
-            return this.name;  
-        };  
-};  
-var personOne = new Person("Diego");  
+function Person(name) {
+    this.name = name;
+    this.getName = function () {
+        return this.name;
+    };
+};
+var personOne = new Person("Diego");
 console.log(personOne.getName()); // prints Diego  
 console.log(personOne instanceof Person); // prints true  
 console.log(personOne.constructor === Person); // prints true  
@@ -57,6 +57,6 @@ console.log(personOne instanceof Object); // prints true
 // Sometimes, you may want to make sure that only a single instance of a certain class exists. 
 // To get a Singleton in Javascript is as simple as defining and invoking the constructor at the same time.
 
-var singleton = new function(){  
-    this.name = "ApplicationName";  
-};  
+var singleton = new function () {
+    this.name = "ApplicationName";
+};
