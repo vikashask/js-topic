01af99ba1c0592c19e07cac145075ffc23bdc8aa
vikashraj144 <vikashraj144@gmail.com>
2018-03-19 09:59:25 +0530

    await added

---
+++ tutorial/Expressions-operators/await.js
@@ -0,0 +1,19 @@
+/* 
+The await expression causes async function execution to pause until a Promise is fulfilled, 
+that is resolved or rejected, and to resume execution of the async function after fulfillment.
+
+If the Promise is rejected, the await expression throws the rejected value.
+*/
+function resolveAfter2Seconds(x) { 
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 2000);
+    });
+  }
+  
+  async function f1() {
+    var x = await resolveAfter2Seconds(10);
+    console.log(x); // 10
+  }
+  f1();
