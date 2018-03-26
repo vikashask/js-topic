568c0006a5fa83def62d6f96fadb1a0a4ef28268
vikashraj144 <vikashraj144@gmail.com>
2018-03-26 15:26:32 +0530

    class

--- class.js
+++ class.js
@@ -17,3 +17,29 @@ var apple = new Apple('fruit');
 apple.color = "yellow";
 console.log(apple.getInfo());
 
+// 2.Using object literals
+
+var water = {
+    type: "macintosh",
+    color: "red",
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+}
+
+water.color="watermalon";
+console.log(water.getInfo);
+
+// 3.Singleton using a function
+
+var apple3 = new function() {
+    this.type = "macintosh";
+    this.color = "red";
+    this.getInfo = function () {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}
+
+apple3.color = "reddish";
+console.log(apple3.getInfo());
+
