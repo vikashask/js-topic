e10bd0f5013973c8b7bfd9597a69e5bcf27a45b1
vikashraj144 <vikashraj144@gmail.com>
2018-03-28 22:05:41 +0530

    json

--- json/json-stringify.js
+++ json/json-stringify.js
@@ -7,3 +7,19 @@ var obj = { "name":"John", "age":30, "city":"New York"};
 var myJSON = JSON.stringify(obj); 
 // myJSON is now a string, and ready to be sent to a server:
 
+// Stringify a JavaScript Array
+var arr = [ "John", "Peter", "Sally", "Jane" ]; 
+var myJSONStr = JSON.stringify(arr); 
+console.log(myJSONStr);
+
+var obj2 = { "name":"John", "today":new Date(), "city":"New York"};
+var myJSON = JSON.stringify(obj2);
+console.log(obj2);
+console.log(myJSON);
+
+// Stringify Functions
+// In JSON, functions are not allowed as object values.
+// The JSON.stringify() function will remove any functions from a JavaScript object, both the key and the value:
+var obj3 = { "name":"John", "age":function () {return 30;}, "city":"New York"};
+var myJSON3 = JSON.stringify(obj3);
+console.log(myJSON3);
