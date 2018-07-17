501937beb865629e0c2991077a788da25ce16c91
vikash verma <vikashraj144@gmail.com>
2018-07-17 14:34:20 +0530

    changes in main

--- tutorial/Value properties/main.js
+++ tutorial/Value properties/main.js
@@ -7,21 +7,21 @@ These global properties return a simple value; they have no properties or method
     null literal
  */
 
-console.log(Infinity          ); /* Infinity */  
-console.log(Infinity + 1      ); /* Infinity */  
-console.log(Math.pow(10, 1000)); /* Infinity */  
-console.log(Math.log(0)       ); /* -Infinity */  
-console.log(1 / Infinity      ); /* 0 */
+console.log(Infinity); /* Infinity */
+console.log(Infinity + 1); /* Infinity */
+console.log(Math.pow(10, 1000)); /* Infinity */
+console.log(Math.log(0)); /* -Infinity */
+console.log(1 / Infinity); /* 0 */
 
-console.log(isNaN(NaN));         // true
-console.log(isNaN(Number.NaN));  // true
+console.log(isNaN(NaN)); // true
+console.log(isNaN(Number.NaN)); // true
 
 var x;
 if (typeof x === 'undefined') {
-   // these statements execute
+    // these statements execute
 }
 
-typeof null          // "object" (not "null" for legacy reasons)
-typeof undefined     // "undefined"
-null === undefined   // false
-null  == undefined   // true
+console.log(typeof null); // "object" (not "null" for legacy reasons)
+console.log(typeof undefined); // "undefined"
+console.log(null === undefined); // false
+console.log(null == undefined); // true
