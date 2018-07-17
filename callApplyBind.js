19f0668d0f767c4cb50bb68c449ed465b0333b4e
vikash verma <vikashraj144@gmail.com>
2018-07-17 14:33:57 +0530

    call apply

--- callApplyBind.js
+++ callApplyBind.js
@@ -1,10 +1,10 @@
-/* 
+/* Call
 It can be used to invoke (call) a function with an owner object as the first argument (parameter).
+With the call() method, you can write a method that can be used on different objects.
  */
 'use strict';
 
 var user = {
-    greet: "Hello!",
     greetUser: function(userName) {
     console.log(this.greet + " " + userName);
     }
@@ -20,6 +20,8 @@ user.greetUser.call(greet1,"vikash") // Output: "Hola vikash"
 apply is nearly the same as call. The only difference is that you pass arguments as an array and 
 not separately Arrays are easier to manipulate in JavaScript
 */
+console.log("------------ apply ----------");
+
 user.greetUser.apply(greet1,["vikash"]) // Output: "Hola vikash"
 
 console.log(Math.max.apply(null,[1,2,3]));
@@ -27,7 +29,7 @@ console.log(Math.max.apply(null,[1,2,3]));
 The bind method allows you to pass arguments to a function without invoking it. 
 A new function is returned with arguments bounded preceding any further arguments
  */
-
+console.log("------------ bind ----------");
 var greetHola = user.greetUser.bind({greet: "Hola"});
 var greetBonjour = user.greetUser.bind({greet: "Bonjour"});
 
