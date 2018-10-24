6a0f721f09b8fdc9167cf597bf85a078116f563d
vikash verma <vikashraj144@gmail.com>
2018-10-24 21:41:18 +0530

    Create hoisting.js
    hoisting added

---
+++ hoisting.js
@@ -0,0 +1,20 @@
+/* 
+Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top
+of their scope before code execution.
+Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope
+(to the top of the current script or the current function).
+*/
+'use strict';
+console.log("example 1");
+let x = 5;
+let y = 10;
+
+console.log("x ="+ x + " ,y ="+y );
+
+console.log("example 2");
+// To avoid bugs, always declare all variables at the beginning of every scope.
+
+let a = 4;
+console.log("a ="+ a + " ,b ="+b );
+let b = 8;
+
