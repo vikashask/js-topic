b90b155b7333205848a681ddde623291a584540b
vikash <vikashraj144@gmail.com>
2019-04-20 18:49:44 +0530

    math

---
+++ math.js
@@ -0,0 +1,12 @@
+Math.abs(x)
+// Returns the absolute value of a number.
+Math.acos(x)
+// Returns the arccosine of a number.
+Math.acosh(x)
+// Returns the hyperbolic arccosine of a number.
+Math.asin(x)
+// Returns the arcsine of a number.
+Math.asinh(x)
+// Returns the hyperbolic arcsine of a number.
+Math.atan(x)
+// Returns the arctangent of a number.
