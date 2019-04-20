00c2b328c58c93b1a46cd2050fe0fc0371fadc26
vikash <vikashraj144@gmail.com>
2019-04-20 18:50:08 +0530

    math method added

--- math.js
+++ math.js
@@ -10,3 +10,35 @@ Math.asinh(x)
 // Returns the hyperbolic arcsine of a number.
 Math.atan(x)
 // Returns the arctangent of a number.
+Math.atanh(x)
+// Returns the hyperbolic arctangent of a number.
+Math.atan2(y, x)
+// Returns the arctangent of the quotient of its arguments.
+Math.cbrt(x)
+// Returns the cube root of a number.
+Math.ceil(x)
+// Returns the smallest integer greater than or equal to a number.
+Math.clz32(x)
+// Returns the number of leading zeroes of a 32-bit integer.
+Math.cos(x)
+// Returns the cosine of a number.
+Math.cosh(x)
+// Returns the hyperbolic cosine of a number.
+Math.exp(x)
+// Returns Ex, where x is the argument, and E is Euler's constant (2.718…), the base of the natural logarithm.
+Math.expm1(x)
+// Returns subtracting 1 from exp(x).
+Math.floor(x)
+// Returns the largest integer less than or equal to a number.
+Math.fround(x)
+// Returns the nearest single precision float representation of a number.
+Math.hypot([x[, y[, …]]])
+// Returns the square root of the sum of squares of its arguments.
+Math.imul(x, y)
+// Returns the result of a 32-bit integer multiplication.
+Math.log(x)
+// Returns the natural logarithm (loge, also ln) of a number.
+Math.log1p(x)
+// Returns the natural logarithm (loge, also ln) of 1 + x for a number x.
+Math.log10(x)
+// Returns the base 10 logarithm of a number.
