81b3708889441204c89186530ce58d949c95caef
vikash <vikashraj144@gmail.com>
2019-04-21 12:59:39 +0530

    lodash added

---
+++ lodash/index.js
@@ -0,0 +1,29 @@
+// Lodash have 
+/*
+Lodash makes JavaScript easier by taking the hassle out of working with arrays, numbers, objects, strings, etc.
+Lodash’s modular methods are great for:
+
+Iterating arrays, objects, & strings
+Manipulating & testing values
+Creating composite functions
+
+npm i --save lodash
+
+// Load the full build.
+var _ = require('lodash');
+
+
+Array
+Collection
+Date
+Function
+Lang
+Math
+Number
+Object
+Seq
+Util
+Properties
+Methods
+
+*/
