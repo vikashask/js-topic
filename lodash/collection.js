b3d2a211dd885502364e5edd45b5ef65bf657c94
vikash <vikashraj144@gmail.com>
2019-04-27 21:47:30 +0530

    collection added

---
+++ lodash/collection.js
@@ -0,0 +1,28 @@
+_.countBy
+_.each //-> forEach
+_.eachRight //-> forEachRight This method is like _.forEach except that it iterates over elements of collection from right to left.
+_.every // Checks if predicate returns truthy for all elements of collection.
+_.filter    // Iterates over elements of collection and invokes iteratee for each element. 
+_.find  // Iterates over elements of collection, returning the first element predicate returns truthy for. 
+_.findLast //This method is like _.find except that it iterates over elements of collection from right to left.
+_.flatMap //
+_.flatMapDeep
+_.flatMapDepth
+_.forEach
+_.forEachRight  // This method is like _.forEach except that it iterates over elements of collection from right to left.
+_.groupBy
+_.includes
+_.invokeMap
+_.keyBy
+_.map
+_.orderBy
+_.partition
+_.reduce
+_.reduceRight
+_.reject
+_.sample
+_.sampleSize
+_.shuffle
+_.size
+_.some
+_.sortBy
