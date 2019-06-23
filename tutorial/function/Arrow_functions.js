0f6450f59b4ddc3ae06521f47ecc771802f305b8
vikash <vikashraj144@gmail.com>
2019-06-23 14:47:42 +0530

    arrow function

--- tutorial/function/Arrow_functions.js
+++ tutorial/function/Arrow_functions.js
@@ -1,4 +1,9 @@
 /* 
+
+In short: context inside arrow functions is lexically or statically defined.
+the value of this inside arrow functions is not dependent on how they are invoked or how they are defined.
+It depends only on its enclosing context.
+
 An arrow function expression has a shorter syntax than a function expression and does not have its
 own this, arguments, super, or new.target. These function expressions are best suited for non-method 
 functions, and they cannot be used as constructors.
@@ -20,11 +25,12 @@ var materials = [
   console.log('---------- by using arrow function ------------');  
   materials.map((material) => {
     console.log(material);
-    // return material.length; // [8, 6, 7, 9]
   }); 
   
   let materialsLength = materials.map(({length}) => length); // [8, 6, 7, 9]
   let materialsArray = materials.map((length) => length); // [8, 6, 7, 9]
   
-  console.log(materialsLength);
+  console.log(materialsLength); // [ 8, 6, 7, 9 ]
+  console.log(materialsArray);  // [ 'Hydrogen', 'Helium', 'Lithium', 'Beryllium' ]
+  
   
