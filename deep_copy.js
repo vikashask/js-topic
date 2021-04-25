075df7d09f6c039e1973fa68fd76c19bce41e7a3
vikash <vikashraj144@gmail.com>
2021-04-25 18:47:08 +0530

    deep copy

---
+++ deep_copy.js
@@ -0,0 +1,10 @@
+/*
+Deep Copy :  Copying nested elements
+
+Deep copy methods
+> JSON.parse(JSON.stringify())
+> service workers postMessage() onmessage
+> History API history.pushState(obj,title) history.state
+> Notification API new  Notification("title,{data:onb}")
+> build a custom recursive function
+*/
