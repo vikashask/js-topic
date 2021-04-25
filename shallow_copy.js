706688fc969d5a27cc1390fb393d99a522b752a9
vikash <vikashraj144@gmail.com>
2021-04-25 18:47:16 +0530

    shallow copy

---
+++ shallow_copy.js
@@ -0,0 +1,13 @@
+/*
+Shallow copy :  Coping on top level elements
+
+Shallow Copy methods examples
+> array.slice(0)
+> [].concat(array)
+> spread operator
+> Object.create({},obj)
+> Object.assign({},obj)
+> Array.from(arr1)
+
+example : https://www.youtube.com/watch?v=mNYo7LTuOyM
+*/
